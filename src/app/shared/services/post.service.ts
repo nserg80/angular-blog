bb0f5ffc8ee14bb851941153994c75dd716340a8
serhii.nyzhnyk <nserg80@gmail.com>
2020-07-16 16:06:20 +0300

    List of posts #1

--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -26,4 +26,16 @@ export class PostService {
       }))
   }
 
+  getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: {[key: string]: any}) => {
+        return Object
+          .keys(response)
+          .map( key => ({
+            ... response[key],
+            id: key,
+            date: new Date(response[key].date)
+          }))
+      }))
+  }
 }
